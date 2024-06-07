de650c49496ee157dbdea1c028247de08a9b6974
Gaston Lopez <gastonlopez436@gmail.com>
2024-06-07 18:59:10 -0300

    update to contentlayer2

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,11 +1,13 @@
-import { defineDocumentType, type ComputedFields, makeSource } from 'contentlayer/source-files'
+import { defineDocumentType, type ComputedFields, makeSource } from 'contentlayer2/source-files'
 import { writeFileSync } from 'fs'
 import readingTime from 'reading-time'
+import { slug } from 'github-slugger'
 import path from 'path'
+import { fromHtmlIsomorphic } from 'hast-util-from-html-isomorphic'
 // Remark packages
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
-import { slug } from 'github-slugger'
+import { remarkAlert } from 'remark-github-blockquote-alert'
 import {
   remarkExtractFrontmatter,
   remarkCodeTitles,
@@ -20,11 +22,23 @@ import rehypeCitation from 'rehype-citation'
 import rehypePrismPlus from 'rehype-prism-plus'
 import rehypePresetMinify from 'rehype-preset-minify'
 import siteMetadata from './data/siteMetadata'
-import { type MDXDocumentDate, allCoreContent, sortPosts } from 'pliny/utils/contentlayer.js'
-import type { Pluggable } from 'unified'
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer.js'
 
 const root = process.cwd()
 const isProduction = process.env.NODE_ENV === 'production'
+
+// heroicon mini link
+const icon = fromHtmlIsomorphic(
+  `
+  <span class="content-header-link">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 linkicon">
+  <path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" />
+  <path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" />
+  </svg>
+  </span>
+`,
+  { fragment: true }
+)
 
 const computedFields: ComputedFields = {
   readingTime: { type: 'json', resolve: (doc) => readingTime(doc.body.raw) },
@@ -40,20 +54,19 @@ const computedFields: ComputedFields = {
     type: 'string',
     resolve: (doc) => doc._raw.sourceFilePath
   },
-  toc: { type: 'string', resolve: async (doc) => await extractTocHeadings(doc.body.raw) }
+  // eslint-disable-next-line @typescript-eslint/promise-function-async
+  toc: { type: 'string', resolve: (doc) => extractTocHeadings(doc.body.raw) }
 }
 
 /**
  * Count the occurrences of all tags across blog posts and write to json file
  */
-
-function createTagCount (allBlogs: any[]) {
+function createTagCount (allBlogs) {
   const tagCount: Record<string, number> = {}
   allBlogs.forEach((file) => {
     // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
     if (file.tags && (!isProduction || file.draft !== true)) {
-      file.tags.forEach((tag: string) => {
-        // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+      file.tags.forEach((tag) => {
         const formattedTag = slug(tag)
         if (formattedTag in tagCount) {
           tagCount[formattedTag] += 1
@@ -63,10 +76,10 @@ function createTagCount (allBlogs: any[]) {
       })
     }
   })
-  writeFileSync('./src/app/tag-data.json', JSON.stringify(tagCount))
+  writeFileSync('./app/tag-data.json', JSON.stringify(tagCount))
 }
 
-function createSearchIndex (allBlogs: any[] | MDXDocumentDate[]) {
+function createSearchIndex (allBlogs) {
   if (
     siteMetadata?.search?.provider === 'kbar' &&
     // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
@@ -91,7 +104,7 @@ export const Blog = defineDocumentType(() => ({
     lastmod: { type: 'date' },
     draft: { type: 'boolean' },
     summary: { type: 'string' },
-    images: { type: 'list', of: { type: 'string' } },
+    images: { type: 'json' },
     authors: { type: 'list', of: { type: 'string' } },
     layout: { type: 'string' },
     bibliography: { type: 'string' },
@@ -109,8 +122,7 @@ export const Blog = defineDocumentType(() => ({
         dateModified: (doc.lastmod != null) || doc.date,
         description: doc.summary,
         image: (doc.images != null) ? doc.images[0] : siteMetadata.socialBanner,
-        url: `${siteMetadata.siteUrl}/${doc._raw.flattenedPath}`,
-        author: doc.authors
+        url: `${siteMetadata.siteUrl}/${doc._raw.flattenedPath}`
       })
     }
   }
@@ -144,11 +156,20 @@ export default makeSource({
       remarkGfm,
       remarkCodeTitles,
       remarkMath,
-      remarkImgToJsx
-    ] as unknown as Pluggable[],
+      remarkImgToJsx,
+      remarkAlert
+    ],
     rehypePlugins: [
       rehypeSlug,
-      rehypeAutolinkHeadings,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'prepend',
+          headingProperties: {
+            className: ['content-header']
+          }
+        }
+      ],
       rehypeKatex,
       [rehypeCitation, { path: path.join(root, 'data') }],
       [rehypePrismPlus, { defaultLanguage: 'js', ignoreMissing: true }],

61788fd029da49c580c38098febaad5bfb5bf40e
GabrielLopez10 <gastonlopez436@gmail.com>
2026-03-23 23:21:29 -0300

    Fix Vulnerability Dependencies 1

--- next.config.js
+++ next.config.js
@@ -79,13 +79,13 @@ module.exports = () => {
         },
       ]
     },
-    turbopack: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
+    webpack: (config, options) => {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      })
+
+      return config
     },
   })
 }
